2e72f84c9ecb79347dfad2997e0ec1244dbf5330
vishal778 <vishalbatra2008@gmail.com>
2021-08-19 13:51:36 +0530

    initialised project

---
+++ App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{backgroundColor: 'red', flex: 1}}>
+      <Text style={{fontSize: 100}}>Demo App</Text>
+    </View>
+  );
+};
+
+export default App;
