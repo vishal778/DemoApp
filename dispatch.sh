634d549e42b29c7a19d684cd6894bfc84f8566f4
vishal778 <vishalbatra2008@gmail.com>
2021-08-20 13:10:09 +0530

    changes 4

--- dispatch.sh
+++ dispatch.sh
@@ -1,4 +1,10 @@
-curl -H "Authorization: token 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" \
-    -H 'Accept: application/vnd.github.everest-preview+json' \
+# curl -H "Authorization: token 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" \
+#     -H 'Accept: application/vnd.github.everest-preview+json' \
+#     "https://api.github.com/repos/vishal778/DemoApp/dispatches" \
+#     -d '{"event_type": "awesomeness", "client_payload": {"foo": "bar"}}'
+
+
+curl -XPOST -u "vishal778" \
+  -H "Content-Type: application/json" \
     "https://api.github.com/repos/vishal778/DemoApp/dispatches" \
     -d '{"event_type": "awesomeness", "client_payload": {"foo": "bar"}}'
